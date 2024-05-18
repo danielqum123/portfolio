d81b7fb3ebbc7e6a66dd6ca1e2fb82b2dd406104
Daniel Qumsieh <daniel_qum@hotmail.com>
2024-05-18 16:54:31 +0300

    less services

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,32 +2,17 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Web design",
-        s_desc:"Developing websites using latest technology stacks."
+        s_desc:"Developing websites using latest technology stacks; mainly React."
     },
     {
         s_no:"02",
-        s_name:"App design",
+        s_name:"Mobile Apps Development",
         s_desc:"Mobile apps development using flutter."
     },
     {
         s_no:"03",
-        s_name:"Graphics design",
-        s_desc:"Creating astonishing logo designs, banners and social media posts."
-    },
-    {
-        s_no:"04",
-        s_name:"Social media",
-        s_desc:"Creating social media content, either visuals or written."
-    },
-    {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Digital marketing services, social media moderation and SEO."
-    },
-    {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Writing content for blogs, social media posts, and short reels."
+        s_name:"Backend Engineering",
+        s_desc:"Building databases and services, integrating APIs and deploying projects to the cloud."
     },
 ]
  
